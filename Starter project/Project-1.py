91837c562ed51507e5e2073193cc92c74bfbb418
jubayed <jubayed.dibbo@gmail.com>
2025-11-27 15:20:12 +0600

    Initial commit on my first project

---
+++ Starter project/Project-1.py
@@ -0,0 +1,33 @@
+import time
+
+def get_input(word_input: str):
+    user_input = input(f"Enter a {word_input}: ")
+    return user_input
+
+
+noun1 = get_input("noun1")
+verb1 = get_input("verb1")
+noun2 = get_input("noun2")
+verb2 = get_input("verb2")
+
+story = f"""
+Once upon a time there was {noun1}, who was a {verb1} teacher
+and his student {noun2} who was a {verb2} programmer. 
+
+One day they were going for institute, and they had a conversation:
+
+{noun1}: Ei chele tmi 1st semester na?
+{noun2}: Assalamu-Aalaikum sir, ji sir!
+{noun1}: Walaikumasslam! tmi etw late keno ajke?
+{noun2}: sorry sir, ami ghum theke uthte late kore felsi! 😭
+{noun1}: lol!
+{noun2}: SIR.....?
+{noun1}: Class e jao!
+{noun2}: Okay sir, lol too!! ☕
+
+Happy Java day everybody!
+"""
+
+for i in story:
+    print(i, end="", flush=True)
+    time.sleep(0.07)
