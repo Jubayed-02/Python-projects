c88d27905a0795fda21d1650f7a9d06f707817a7
jubayed <jubayed.dibbo@gmail.com>
2026-02-11 21:37:33 +0600

    initial commit

---
+++ Starter project/updated-Project-5.py
@@ -0,0 +1,53 @@
+import random 
+
+def RPS():
+    # welcome message for the player
+    print("Welcome to Rock-Paper-Scissors game!")
+
+    # player and bot point will store here
+    bot_point = 0
+    player_point = 0
+
+    while True:
+        player_move: str = input("rock/paper/scissors >> ").lower() # user input in lower case !!!
+        valid_moves = ["rock", "paper", "scissors"] # valid moves of the game
+        winning_con = {"rock": "scissors", "paper":"rock", "scissors": "paper"} #lookup table for now
+
+        bot_move = random.choice(valid_moves) # a random move for the bot
+
+        # to exit the game
+        if player_move == "exit":
+            print("\nThanks for playing the game!\nHave a nice day!")
+            break
+
+        # to check the input is valid or not
+        if player_move not in valid_moves:
+            print("invalid move!")
+            continue
+        
+        print("-"*20) # for styling
+
+        # displaying moves
+        print(f"You: {player_move}") # player's move
+        print(f"bot move: {bot_move}") # bot's move 
+
+        # game condition
+        if player_move == bot_move:
+            print("It's a tie!")
+        elif winning_con[player_move] == bot_move:
+            print("You won!")
+            player_point += 1
+        else:
+            print("bot won!")
+            bot_point += 1
+
+        # score 
+        print(f"\nPlayer's point: {player_point}")
+        print(f"Bot point: {bot_point}")    
+        
+        print("** enter 'exit' to quit the game anytime you want! **") # condition to quit the game
+        print("-"*20) # for styling
+
+
+if __name__=="__main__":
+    RPS()
